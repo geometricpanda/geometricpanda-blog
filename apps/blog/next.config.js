6d8e1c5f64d1389a00f430fdef6fad25b544d1b2
Jim Drury <jim@jimdrury.co.uk>
2021-06-06 11:26:27 +0100

    feat(pipeline): adjusts pipeline steps for next to run in netlify (instead of static)

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -5,6 +5,6 @@ module.exports = withNx({
   nx: {
     // Set this to false if you do not want to use SVGR
     // See: https://github.com/gregberge/svgr
-    svgr: true,
-  },
+    svgr: true
+  }
 });
