8fef2145201ed873193d06aa6abfdfd98f07e46e
Jim Drury <jim@jimdrury.co.uk>
2021-06-06 16:34:38 +0100

    fix(title): resolves issue with title flickering

--- apps/blog/pages/articles/[year]/[month]/[day]/[slug].tsx
+++ apps/blog/pages/articles/[year]/[month]/[day]/[slug].tsx
@@ -1,20 +1,23 @@
 import React, { FC, useEffect, useState } from 'react';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { formatISO, parse } from 'date-fns';
+
 import {
   Container,
   FreeText,
   Image,
   PageHeader,
 } from '@geometricpanda/react-components';
-import { formatISO, parse } from 'date-fns';
+
+import type { Article } from '../../../../../contentful/interface';
 
 import {
   getArticle,
   getArticleSlugs,
 } from '../../../../../contentful/articles';
+
 import { getFullSlugFromArticle } from '../../../../../lib/get-full-slug-from-article';
 
-import type { Article } from '../../../../../contentful/interface';
 import Head from 'next/head';
 
 export async function getStaticProps({ params }) {
@@ -43,21 +46,12 @@ interface PageProps {
 }
 
 const Page: FC<PageProps> = ({ article }) => {
-  const [publishedDate, setPublishedDate] = useState<Date>();
-  const [preview, setPreview] = useState<string>('');
-  const [content, setContent] = useState<string>('');
-  const [title, setTitle] = useState<string>('');
-  const [heroImg, setHeroImg] = useState<string>('');
-  const [heroAlt, setHeroAlt] = useState<string>('');
-
-  useEffect(() => {
-    setTitle(article.title);
-    setPublishedDate(new Date(article.sys.firstPublishedAt));
-    setPreview(documentToHtmlString(article.intro.json));
-    setContent(documentToHtmlString(article.content.json));
-    setHeroAlt(article.hero.title);
-    setHeroImg(article.hero.url);
-  }, [article]);
+  const publishedDate = new Date(article.sys.firstPublishedAt);
+  const preview = documentToHtmlString(article.intro.json);
+  const content = documentToHtmlString(article.content.json);
+  const title = article.title;
+  const heroImg = article.hero.url;
+  const heroAlt = article.hero.title;
 
   return (
     <>
@@ -68,7 +62,7 @@ const Page: FC<PageProps> = ({ article }) => {
         <article>
           <PageHeader date={publishedDate}>{title}</PageHeader>
           <Image height={150} width={300} alt={heroAlt} src={heroImg} mb />
-          <FreeText html={`${preview}${content}`} />
+          <FreeText boldParagraph html={`${preview}${content}`} />
         </article>
       </Container>
     </>
