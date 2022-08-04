8ecce15557a44978369aa8d7c000bf80c6692113
Jim Drury <jim.drury@virginmediao2.co.uk>
2022-08-04 15:24:59 +0100

    fix(editor-experience): wraps separator in container to give appropriate highlight area to storyblok

--- common/components/bloks/separator-blok/index.tsx
+++ common/components/bloks/separator-blok/index.tsx
@@ -2,6 +2,7 @@ import {storyblokEditable} from '@storyblok/react';
 import type {SbBlokData} from '@storyblok/js/dist/types/types';
 import type {FC} from 'react';
 
+import {Container} from '../../container';
 import styles from './index.module.css';
 
 export interface SeparatorBlokInterface extends SbBlokData {
@@ -12,8 +13,8 @@ export interface SeparatorBlokProps {
 }
 
 export const SeparatorBlok: FC<SeparatorBlokProps> = ({blok}) => (
-  <div {...storyblokEditable(blok)}>
+  <Container {...storyblokEditable(blok)}>
     <hr className={styles['separator']}/>
-  </div>
+  </Container>
 )
 
