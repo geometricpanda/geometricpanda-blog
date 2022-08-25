b9b7eb36469be3e1a01383cfe9f01595ca10e3c9
Jim Drury <jim.drury@virginmediao2.co.uk>
2022-08-25 21:54:40 +0100

    feat(prism): adds additional languages and grammars

--- common/helpers/syntax-highlighter.ts
+++ common/helpers/syntax-highlighter.ts
@@ -1,4 +1,11 @@
 import Prism from 'prismjs';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-tsx';
+import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-bash';
 
 export enum LANGUAGE {
   HTML = 'html',
@@ -15,19 +22,19 @@ export enum LANGUAGE {
 export const syntaxHighlighter = (code: string, language: LANGUAGE) => {
   switch (language.toLowerCase()) {
     case LANGUAGE.JSON:
-      return Prism.highlight(code, Prism.languages.javascript, 'json');
+      return Prism.highlight(code, Prism.languages.json, 'json');
     case LANGUAGE.JAVASCRIPT:
       return Prism.highlight(code, Prism.languages.javascript, 'javascript');
     case LANGUAGE.TYPESCRIPT:
-      return Prism.highlight(code, Prism.languages.javascript, 'typescript');
+      return Prism.highlight(code, Prism.languages.typescript, 'typescript');
     case LANGUAGE.TSX:
-      return Prism.highlight(code, Prism.languages.javascript, 'tsx');
+      return Prism.highlight(code, Prism.languages.tsx, 'tsx');
     case LANGUAGE.JSX:
-      return Prism.highlight(code, Prism.languages.javascript, 'jsx');
+      return Prism.highlight(code, Prism.languages.jsx, 'jsx');
     case LANGUAGE.CSS:
       return Prism.highlight(code, Prism.languages.css, 'css');
     case LANGUAGE.SCSS:
-      return Prism.highlight(code, Prism.languages.css, 'scss');
+      return Prism.highlight(code, Prism.languages.scss, 'scss');
     case LANGUAGE.HTML:
       return Prism.highlight(code, Prism.languages.html, 'html');
     case LANGUAGE.BASH:
